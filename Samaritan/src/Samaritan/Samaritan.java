fa873f30d0837c53c5a4c8cebd8f016399a6bd10
the-pubisher <https://10morgant%40gmail.com>
2017-06-29 14:04:07 +0100

    samaritian frame

--- Samaritan/src/Samaritan/Samaritan.java
+++ Samaritan/src/Samaritan/Samaritan.java
@@ -4,20 +4,40 @@
  * and open the template in the editor.
  */
 package Samaritan;
+
 import Periscope.Periscope;
+
 /**
- *
  * @author 160165
  */
 public class Samaritan {
+    Periscope p;
+
+    Samaritan() {
+        StartUpFunctions startUpFunctions = new StartUpFunctions();
+        startUpFunctions.initialise(p);
+        this.p = startUpFunctions.getPeriscope();
+        p.main();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        Periscope p = new Periscope();
-        Periscope.main();
+        new Samaritan();
     }
-    
+
+    class StartUpFunctions {
+        Periscope p;
+
+        void initialise(Periscope p) {
+            p = new Periscope();
+            this.p = p;
+            System.out.println("This is starting up");
+        }
+
+        public Periscope getPeriscope() {
+            return p;
+        }
+    }
 }
