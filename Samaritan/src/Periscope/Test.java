989eea98e4a06a78532f2d41df3e636bbe4d90b3
Mathew <Mathew@DESKTOP-C94MAVS>
2017-07-01 23:51:41 +0100

    Got negatives of an image working.

--- Samaritan/src/Periscope/Test.java
+++ Samaritan/src/Periscope/Test.java
@@ -4,25 +4,151 @@
  * and open the template in the editor.
  */
 package Periscope;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.CvType;
-import org.opencv.core.Scalar;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-class Test {
+import org.opencv.core.*;
 
-  static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+import org.opencv.videoio.VideoCapture;        
 
-  public static void main(String[] args) {
-    System.out.println("Welcome to OpenCV " + Core.VERSION);
-    Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-    System.out.println("OpenCV Mat: " + m);
-    Mat mr1 = m.row(1);
-    mr1.setTo(new Scalar(1));
-    Mat mc5 = m.col(5);
-    mc5.setTo(new Scalar(5));
-    System.out.println("OpenCV Mat data:\n" + m.dump());
-  }
+public class Test extends JPanel{
+
+    BufferedImage image;
+
+    public static void main (String args[]) throws InterruptedException{
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Test t = new Test();
+        VideoCapture camera = new VideoCapture(0);
+
+        Mat frame = new Mat();
+        camera.read(frame); 
+
+        if(!camera.isOpened()){
+            System.out.println("Error");
+        }
+        else {                  
+            while(true){        
+
+                if (camera.read(frame)){
+
+                    BufferedImage image = t.MatToBufferedImage(frame);
+
+                    t.window(image, "Original Image", 0, 0);
+
+                    t.window(t.grayscale(image), "Processed Image", 40, 60);
+
+                    //t.window(t.loadImage("ImageName"), "Image loaded", 0, 0);
+
+                    break;
+                }
+            }   
+        }
+        camera.release();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(image, 0, 0, this);
+    }
+
+    public Test() {
+    }
+
+    public Test(BufferedImage img) {
+        image = img;
+    }   
+
+    //Show image on window
+    public void window(BufferedImage img, String text, int x, int y) {
+        JFrame frame0 = new JFrame();
+        frame0.getContentPane().add(new Test(img));
+        frame0.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame0.setTitle(text);
+        frame0.setSize(img.getWidth(), img.getHeight() + 30);
+        frame0.setLocation(x, y);
+        frame0.setVisible(true);
+    }
+
+    //Load an image
+    public BufferedImage loadImage(String file) {
+        BufferedImage img;
+
+        try {
+            File input = new File(file);
+            img = ImageIO.read(input);
+
+            return img;
+        } catch (Exception e) {
+            System.out.println("erro");
+        }
+
+        return null;
+    }
+
+    //Save an image
+    public void saveImage(BufferedImage img) {        
+        try {
+            File outputfile = new File("Images/new.png");
+            ImageIO.write(img, "png", outputfile);
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
+
+    //Grayscale filter
+    public BufferedImage grayscale(BufferedImage img) {
+        for (int i = 0; i < img.getHeight(); i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                Color c = new Color(img.getRGB(j, i));
+
+                int red = (int) (c.getRed() * 0.299);
+                int green = (int) (c.getGreen() * 0.587);
+                int blue = (int) (c.getBlue() * 0.114);
+                int finalVal = 0;
+                if((red+green+blue)>(127)) finalVal = 255;
+                 
+                Color newColor =
+                        new Color(
+                        finalVal,
+                        finalVal,
+                        finalVal);
+
+                img.setRGB(j, i, newColor.getRGB());
+            }
+        }
+
+        return img;
+    }
+
+    public BufferedImage MatToBufferedImage(Mat frame) {
+        //Mat() to BufferedImage
+        int type = 0;
+        if (frame.channels() == 1) {
+            type = BufferedImage.TYPE_BYTE_GRAY;
+        } else if (frame.channels() == 3) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        BufferedImage image = new BufferedImage(frame.width(), frame.height(), type);
+        WritableRaster raster = image.getRaster();
+        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+        byte[] data = dataBuffer.getData();
+        frame.get(0, 0, data);
+
+        return image;
+    }
 
 }
