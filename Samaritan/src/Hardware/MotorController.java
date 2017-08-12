890d3dabe8c81731702484a1e0923e4b2dbd72f9
mathew <mathew@10.59.0.17>
2017-08-12 12:41:43 +0100

    do

--- Samaritan/src/Hardware/MotorController.java
+++ Samaritan/src/Hardware/MotorController.java
@@ -10,10 +10,26 @@ package Hardware;
  * @author mathew
  */
 import com.fazecast.jSerialComm.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 public class MotorController {
-    public MotorController(){
+    public MotorController() throws IOException{
+        SerialPort[] serialArray = SerialPort.getCommPorts();
         
-        
+       for(SerialPort p: serialArray) System.out.println(p.getDescriptivePortName()+" == "+p.getSystemPortName());
+       serialArray[5].openPort();
+       serialArray[5].setBaudRate(9600);
+       
+      InputStream st = serialArray[5].getInputStream();
+      BufferedInputStream r = new BufferedInputStream(st);
+      while(true){
+      while(st.available()>0){
+          System.out.print((char)r.read());
+      }
+      }
     }
     public void halt(){
         
@@ -23,6 +39,10 @@ public class MotorController {
     }
     public void setMotor(int motNum, int motorPower){
         
+    }
+    
+    public static void main(String args[]) throws IOException{
+        MotorController m = new MotorController();
     }
     
 }
