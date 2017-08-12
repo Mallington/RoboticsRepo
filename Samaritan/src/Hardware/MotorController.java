78107c307264d08df8e7899962bd782d660c379d
mathew <mathew@10.59.0.17>
2017-08-12 15:36:38 +0100

    dom

--- Samaritan/src/Hardware/MotorController.java
+++ Samaritan/src/Hardware/MotorController.java
@@ -11,12 +11,16 @@ package Hardware;
  */
 import com.fazecast.jSerialComm.*;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 public class MotorController {
-    public MotorController() throws IOException{
+    public MotorController() throws IOException, InterruptedException{
+        /*
         SerialPort[] serialArray = SerialPort.getCommPorts();
         
        for(SerialPort p: serialArray) System.out.println(p.getDescriptivePortName()+" == "+p.getSystemPortName());
@@ -29,7 +33,34 @@ public class MotorController {
       while(st.available()>0){
           System.out.print((char)r.read());
       }
-      }
+      }*/
+        
+        SerialPort[] serialArray = SerialPort.getCommPorts();
+        int i =0;
+         for(SerialPort p: serialArray) System.out.println(p.getDescriptivePortName()+" == "+p.getSystemPortName()+" port: "+(i++));
+         int port = 5;
+         serialArray[port].openPort();
+         OutputStream out = serialArray[port].getOutputStream();
+         
+         
+         
+         
+         BufferedOutputStream outs = new BufferedOutputStream(out);
+        Thread.sleep(5000);
+         outs.write(49);
+         outs.write(55);
+         outs.flush();
+         outs.close();
+         BufferedInputStream sin = new BufferedInputStream(serialArray[port].getInputStream());
+         
+         while(true){
+             while(sin.available()>0){
+                 System.out.print((char)sin.read());
+             }
+         }
+         
+         
+         
     }
     public void halt(){
         
@@ -38,10 +69,13 @@ public class MotorController {
         
     }
     public void setMotor(int motNum, int motorPower){
+        List<Integer> params = new ArrayList<Integer>();
+        params.add(motNum);
         
     }
     
-    public static void main(String args[]) throws IOException{
+    
+    public static void main(String args[]) throws IOException, InterruptedException{
         MotorController m = new MotorController();
     }
     
